004153a8919b365b0b5689e22dd36a9ab5872ba3
Phillip Matambanadzo <phillip.matambanadzo@kisscoders.ruhr>
2023-09-01 12:46:50 +0100

    Implement Reference Section

---
+++ src/components/ReferenceSection/ReferenceSection.vue
@@ -0,0 +1,62 @@
+<template>
+  <section
+    class="pt-12 pb-24 max-w-screen-lg mx-auto flex flex-col items-center"
+  >
+    <h2 class="text-center font-exo-2 font-bold text-4xl">
+      Don't just take my word for it
+    </h2>
+    <hr class="w-24 h-px my-4 bg-red-400 border-0" />
+    <p class="text-center font-light">
+      Hear from those who've experienced my craft firsthand.
+    </p>
+    <div class="grid grid-cols-2 gap-12 max-w-screen-md mt-12">
+      <div class="relative px-4 pt-8">
+        <span
+          class="text-9xl font-exo-2 font-bold absolute top-0 left-0 text-gray-700"
+          >“</span
+        >
+        <div class="w-24 h-24 bg-gray-400 rounded-full float-right ml-4"></div>
+        <p class="text-justify mt-10 text-gray-600">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <div class="flex flex-row mt-4">
+          <p class="font-normal">-&nbsp;</p>
+          <div>
+            <p class="font-normal">David Müller</p>
+            <p class="text-sm">Founder & Senior Software Architext - Hakobu</p>
+          </div>
+        </div>
+      </div>
+      <div class="relative px-4 pt-8">
+        <span
+          class="text-9xl font-exo-2 font-bold absolute top-0 left-0 text-gray-700"
+          >“</span
+        >
+        <div class="w-24 h-24 bg-gray-400 rounded-full float-right ml-4"></div>
+        <p class="text-justify mt-10 text-gray-600">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <div class="flex flex-row mt-4">
+          <p class="font-normal">-&nbsp;</p>
+          <div>
+            <p class="font-normal">David Müller</p>
+            <p class="text-sm">Founder & Senior Software Architext - Hakobu</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script lang="ts" setup></script>
