825c682203507141fad3a3599f94ff8c6f7ece98
Phillip Matambanadzo <phillip.matambanadzo@kisscoders.ruhr>
2023-09-01 02:04:37 +0100

    implement hero and projects

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,3 +1,64 @@
 <template>
-  <div>TestCard</div>
+  <div
+    class="rounded-lg relative before:absolute before:w-full before:top-0 before:border before:border-red-400 before:border-b-0 before:h-4 before:rounded-t-lg after:absolute after:w-full after:bottom-0 after:border after:border-red-400 after:border-t-0 after:h-4 after:rounded-b-lg"
+  >
+    <div class="p-4">
+      <h3 class="font-medium text-lg">
+        Senior Frontend Engineer | Mercedes-Benz.io
+      </h3>
+      <p class="text-gray-500">01/2022 - 09/2022 (1 year, 9 months)</p>
+      <p class="my-6">
+        Together with an international team of frontend, backend and devops
+        engineers, I've contributed to the development of the Mercedes Me web
+        storefront.
+      </p>
+      <div class="flex flex-wrap">
+        <div
+          class="mr-1 mt-1 rounded-full bg-red-100 font-bold text-sm text-red-400 inline-flex h-7 items-center px-4"
+        >
+          Vue
+        </div>
+        <div
+          class="mr-1 mt-1 rounded-full bg-red-100 font-bold text-sm text-red-400 inline-flex h-7 items-center px-4"
+        >
+          Typescript
+        </div>
+        <div
+          class="mr-1 mt-1 rounded-full bg-red-100 font-bold text-sm text-red-400 inline-flex h-7 items-center px-4"
+        >
+          Pinia
+        </div>
+        <div
+          class="mr-1 mt-1 rounded-full bg-red-100 font-bold text-sm text-red-400 inline-flex h-7 items-center px-4"
+        >
+          VueX
+        </div>
+        <div
+          class="mr-1 mt-1 rounded-full bg-red-100 font-bold text-sm text-red-400 inline-flex h-7 items-center px-4"
+        >
+          Vite
+        </div>
+        <div
+          class="mr-1 mt-1 rounded-full bg-red-100 font-bold text-sm text-red-400 inline-flex h-7 items-center px-4"
+        >
+          Jenkins
+        </div>
+        <div
+          class="mr-1 mt-1 rounded-full bg-red-100 font-bold text-sm text-red-400 inline-flex h-7 items-center px-4"
+        >
+          Vitest
+        </div>
+        <div
+          class="mr-1 mt-1 rounded-full bg-red-100 font-bold text-sm text-red-400 inline-flex h-7 items-center px-4"
+        >
+          Github
+        </div>
+        <div
+          class="mr-1 mt-1 rounded-full bg-red-100 font-bold text-sm text-red-400 inline-flex h-7 items-center px-4"
+        >
+          Kubernetes
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
