ee231179a2ec3232d84305c2b297be451258bf8b
Phillip Matambanadzo <phillip.matambanadzo@kisscoders.ruhr>
2023-09-01 14:37:15 +0100

    change nav color when hovering dark bg

--- src/components/StackSection/StackSection.vue
+++ src/components/StackSection/StackSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="bg-gray-900 pt-12 pb-24">
+  <section ref="rootElement" class="bg-gray-900 pt-12 pb-24">
     <div class="max-w-screen-sm mx-auto flex flex-col items-center">
       <h2 class="text-center font-exo-2 font-bold text-4xl text-white">
         Tech Stack
@@ -20,28 +20,34 @@
   </section>
 </template>
 <script lang="ts" setup>
+import { onMounted, onUnmounted, ref } from "vue";
 import StackSection from "./StackSectionIcon/StackSectionIcon.vue";
-const ICONS = [
-  "vue",
-  "react",
-  "html",
-  "css",
-  "javascript",
-  "typescript",
-  "node",
-  "git",
-  "aws",
-  "kubernetes",
-  "mongodb",
-  "docker",
-];
-/* const itemClass = (index: number) => {
-  let baseClass = "w-full"; // default for small screens: 1 item per row
-  if (index < 7) {
-    baseClass = "md:w-[calc(100%/7)] lg:w-1/7"; // 1/4 width on medium screens, 1/7 on large for the first 7 items
-  } else if (index < 12) {
-    baseClass = "md:w-1/5 lg:w-1/5"; // 1/4 width on medium screens, 1/5 on large for the next 5 items
-  }
-  return baseClass;
-}; */
+import { ICONS } from "./enums";
+
+const emit = defineEmits(["isIntersecting"]);
+
+const rootElement = ref(null);
+let observer: IntersectionObserver | null = null;
+
+onMounted(() => {
+  console.log("mounted");
+  const options = {
+    root: null,
+    rootMargin: "0px 0px -100% 0px",
+    threshold: 0,
+  };
+  observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        emit("isIntersecting", true);
+      } else {
+        emit("isIntersecting", false);
+      }
+    });
+  }, options);
+  observer.observe(rootElement.value!);
+});
+onUnmounted(() => {
+  observer?.disconnect();
+});
 </script>
---
+++ src/components/StackSection/enums/index.ts
@@ -0,0 +1,14 @@
+export const ICONS = [
+  "vue",
+  "react",
+  "html",
+  "css",
+  "javascript",
+  "typescript",
+  "node",
+  "git",
+  "aws",
+  "kubernetes",
+  "mongodb",
+  "docker",
+];
