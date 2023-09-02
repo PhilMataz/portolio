ca7c78e88762f2aba807063e9ee2fd5be42c3c3d
Phillip Matambanadzo <phillip.matambanadzo@kisscoders.ruhr>
2023-09-02 21:54:08 +0100

    Make hoverAnimation available as composable

--- src/components/BaseLink.vue
+++ src/components/BaseLink.vue
@@ -10,7 +10,7 @@
   >
 </template>
 <script lang="ts" setup>
-import { ref } from "vue";
+import { useHoverAnimation } from "../composables/useHoverAnimation";
 
 interface Props {
   title: string;
@@ -19,43 +19,18 @@ interface Props {
   scrollTo?: boolean;
 }
 
-const LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567891" as const;
-
 const emit = defineEmits(["click"]);
-let interval: number | null = null;
 
 const props = withDefaults(defineProps<Props>(), {
   target: "_self",
   scrollTo: false,
 });
-const displayValue = ref(props.title);
+
+const { displayValue, handleMouseOver, handleMouseLeave } = useHoverAnimation(
+  props.title
+);
 
 const handleClick = (event: MouseEvent) => {
   emit("click", event);
-};
-
-const handleMouseOver = () => {
-  let iterations = 0;
-  interval = setInterval(() => {
-    displayValue.value = props.title
-      .split("")
-      .map((_, index) => {
-        if (index < iterations) {
-          return props.title[index];
-        }
-        return LETTERS[Math.floor(Math.random() * LETTERS.length)];
-      })
-      .join("");
-    if (iterations >= props.title.length && interval) {
-      clearInterval(interval);
-    }
-    iterations += 1 / 2;
-  }, 30);
-};
-const handleMouseLeave = () => {
-  displayValue.value = props.title;
-  if (interval) {
-    clearInterval(interval);
-  }
 };
 </script>
---
+++ src/composables/useHoverAnimation.ts
@@ -0,0 +1,40 @@
+import { ref } from "vue";
+
+const LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567891" as const;
+
+export const useHoverAnimation = (initialValue: string) => {
+  let interval: number | null = null;
+  const displayValue = ref(initialValue);
+
+  const handleMouseOver = () => {
+    let iterations = 0;
+    interval = setInterval(() => {
+      displayValue.value = initialValue
+        .split("")
+        .map((_, index) => {
+          if (index < iterations) {
+            return initialValue[index];
+          }
+          return LETTERS[Math.floor(Math.random() * LETTERS.length)];
+        })
+        .join("");
+      if (iterations >= initialValue.length && interval) {
+        clearInterval(interval);
+      }
+      iterations += 1 / 2;
+    }, 30);
+  };
+
+  const handleMouseLeave = () => {
+    displayValue.value = initialValue;
+    if (interval) {
+      clearInterval(interval);
+    }
+  };
+
+  return {
+    displayValue,
+    handleMouseOver,
+    handleMouseLeave,
+  };
+};
