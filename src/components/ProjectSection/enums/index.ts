89ccf2e34c5bc035b7beaffd55e67ede2aeb6a7a
Phillip Matambanadzo <phillip.matambanadzo@kisscoders.ruhr>
2024-06-07 09:39:51 +0100

    feat: update projects

--- src/components/ProjectSection/enums/index.ts
+++ src/components/ProjectSection/enums/index.ts
@@ -1,11 +1,30 @@
 export const PROJECTS = [
   {
+    name: 'Senior Fullstack Engineer | SMG',
+    date: '11/2023 - today',
+    description:
+      'In this role, I collaborated with an international team of full-stack and DevOps engineers to enhance the salesfunnel of a leading real estate platform (homegate.ch and immoscout24.ch). I was responsible for modernizing the frontend and backend stacks to meet contemporary standards. Beyond technical upgrades, I played a key role in planning and executing the development of new features.',
+    skills: [
+      'Vue',
+      'Typescript',
+      'VueX',
+      'Jest',
+      'GitHub',
+      'AWS Lambda',
+      'CKD',
+      'Serverless',
+      'Node.js',
+      'DataDog',
+    ],
+  },
+  {
     name: 'Senior Frontend Engineer | Mercedes-Benz.io',
     date: '01/2022 - 09/2023 (1 year, 9 months)',
     description:
       'Together with an international team of frontend, backend, and devops engineers, I played a key role in modernizing the Mercedes Connect web store. During this endeavor, I led the transition from Vue 2 to Vue 3 and introduced TypeScript, ensuring a more robust and scalable platform.',
     skills: [
       'Vue',
+      'Nuxt',
       'Typescript',
       'Pinia',
       'VueX',
@@ -14,6 +33,7 @@ export const PROJECTS = [
       'Vitest',
       'Github',
       'Kubernetes',
+      'DataDog',
     ],
   },
   {
@@ -39,23 +59,5 @@ export const PROJECTS = [
     description:
       'In my role, I designed and implemented a scheduler for a Digital Signage CMS. This tool was crafted to enable users to efficiently plan, schedule, and upload advertisement campaigns. Once these campaigns were set, they were showcased on multimedia screens across different venues.',
     skills: ['Vue', 'Typescript', 'VueX', 'Meteor', 'Node.js', 'Jest', 'MongoDB'],
-  },
-  {
-    name: 'Fullstack Engineer | Westend Druckereibetriebe',
-    date: '01/2018 - 07/2021 (3 year, 6 months)',
-    description:
-      "At a leading printing company, I played a pivotal role in optimizing their digital operations. I designed and implemented an order application, which became essential for client interactions. Furthermore, I established an extensive testing suite, guaranteeing the software's performance and robustness.",
-    skills: [
-      'Vue',
-      'Javascript',
-      'React',
-      'VueX',
-      'Cypress',
-      'Jest',
-      'Docker',
-      'Node.js',
-      'Meteor',
-      'MongoDB',
-    ],
   },
 ];
